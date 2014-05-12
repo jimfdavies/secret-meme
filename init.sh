3f70052a61f216cd3013a4bc47b220475e65250b
Jim Davies <jimfdavies@googlemail.com>
2014-05-12 22:21:47 +0100

    add init files

---
+++ init.sh
@@ -0,0 +1,11 @@
+# Initially using boot2docker on VirtualBox
+# After booting this VM, setup port forwarding 2222:22
+
+ssh docker@127.0.0.1 -p 2222
+# tcuser
+git clone https://github.com/jimfdavies/secret-meme.git
+cd secret-meme
+git checkout master
+
+chmod a+x docker-init.sh
+./docker-init.sh
