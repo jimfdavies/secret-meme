b33e6f05c6135e8f1f93b77c3f431e01ec58c21e
Jim Davies <jimfdavies@googlemail.com>
2014-05-20 22:30:16 +0100

    added initial docker-jenkins

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 
 git clone https://github.com/jimfdavies/secret-meme.git
-cd secret-meme
-
-docker build --rm=true --tag=hubot docker-hubot/
+cd /secret-meme
+docker build --rm=true --name=jenkins docker-jenkins/
